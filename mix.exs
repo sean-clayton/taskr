623876c9803180ae9825ce1f3de0f18524172e9e
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2018-12-23 19:23:09 -1000

    Add new deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule Taskr.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:eventstore, "~> 0.15.1"},
+      {:commanded, "~> 0.17.2"},
+      {:commanded_eventstore_adapter, "~> 0.4.0"}
     ]
   end
 
